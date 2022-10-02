ad164f151af16221cfc42293224692e0dc45a1af
IttyBittyNinja <j4reda@gmail.com>
2022-10-02 18:03:16 -0400

    Initial Commit

---
+++ [ABRAMS] ACTIVITY 5/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace _ABRAMS__ACTIVITY_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.filePathInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.firstWord = new System.Windows.Forms.TextBox();
+            this.lastWord = new System.Windows.Forms.TextBox();
+            this.vowelWord = new System.Windows.Forms.TextBox();
+            this.wordListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFile";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // filePathInput
+            // 
+            this.filePathInput.Location = new System.Drawing.Point(81, 0);
+            this.filePathInput.Name = "filePathInput";
+            this.filePathInput.Size = new System.Drawing.Size(308, 22);
+            this.filePathInput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "All Words In Lower-case";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "First Word (Alphabetically)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Last Word (Alphabetically)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Word With Most Vowels";
+            // 
+            // firstWord
+            // 
+            this.firstWord.Location = new System.Drawing.Point(182, 183);
+            this.firstWord.Name = "firstWord";
+            this.firstWord.Size = new System.Drawing.Size(207, 22);
+            this.firstWord.TabIndex = 6;
+            // 
+            // lastWord
+            // 
+            this.lastWord.Location = new System.Drawing.Point(182, 217);
+            this.lastWord.Name = "lastWord";
+            this.lastWord.Size = new System.Drawing.Size(207, 22);
+            this.lastWord.TabIndex = 7;
+            // 
+            // vowelWord
+            // 
+            this.vowelWord.Location = new System.Drawing.Point(182, 246);
+            this.vowelWord.Name = "vowelWord";
+            this.vowelWord.Size = new System.Drawing.Size(207, 22);
+            this.vowelWord.TabIndex = 8;
+            // 
+            // wordListBox
+            // 
+            this.wordListBox.FormattingEnabled = true;
+            this.wordListBox.ItemHeight = 16;
+            this.wordListBox.Location = new System.Drawing.Point(182, 44);
+            this.wordListBox.Name = "wordListBox";
+            this.wordListBox.Size = new System.Drawing.Size(207, 116);
+            this.wordListBox.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 279);
+            this.Controls.Add(this.wordListBox);
+            this.Controls.Add(this.vowelWord);
+            this.Controls.Add(this.lastWord);
+            this.Controls.Add(this.firstWord);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filePathInput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Word Sorter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox filePathInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox firstWord;
+        private System.Windows.Forms.TextBox lastWord;
+        private System.Windows.Forms.TextBox vowelWord;
+        private System.Windows.Forms.ListBox wordListBox;
+    }
+}
+
---
+++ [ABRAMS] ACTIVITY 5/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _ABRAMS__ACTIVITY_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader inputFile;
+            StreamWriter outputFile;
+
+            if (openFile.ShowDialog() == DialogResult.OK) // Open the selected file
+            {
+                filePathInput.Text = openFile.FileName; // Display file directory in textbox
+                inputFile = new StreamReader(openFile.FileName); // Set input file to selected file path
+                outputFile = File.CreateText("Output.txt"); // Create output file
+                wordListBox.Items.Clear(); // Clear anything currently in the ListBox
+                
+                string wordList; // Current word being read from file
+                string firstWordSort = "z"; // First word alphabetically
+                string lastWordSort = "a"; // Last word alphabetically
+                string vowelWordSort = ""; // Word with most vowels
+                int vowelCountFinal = 0; // Vowel counter
+
+                while (!inputFile.EndOfStream)
+                {
+                    wordList = inputFile.ReadLine(); // Get word from file list
+                    
+                    wordListBox.Items.Add(wordList.ToLower()); // print word to textbox in lower-case
+                    outputFile.WriteLine(wordList.ToLower());
+
+
+                    if (String.Compare(wordList.ToLower(), firstWordSort.ToLower()) < 0) // Compare to strings to find the first word alphabetically
+                    {
+                        firstWordSort = wordList;
+                    }
+                    firstWord.Text = firstWordSort; // Print first word to textbox
+                    
+
+                    if (String.Compare(wordList.ToLower(), lastWordSort.ToLower()) > 0) // Compare to strings to find the last word alphabetically
+                    {
+                        lastWordSort = wordList;
+                    }
+                    lastWord.Text = lastWordSort; // Print last word to textbox
+                    
+
+                    int vowelCount = 0; // vowel counter for for-loop
+                    for (int i = 0; i < wordList.Length; i++) // for-loop based on length of word
+                    {
+                        if (wordList.ToLower()[i] == 'a' || wordList.ToLower()[i] == 'e' || wordList.ToLower()[i] == 'i' || wordList.ToLower()[i] == 'o' || wordList.ToLower()[i] == 'u') // If the current character is a vowel
+                        {
+                            vowelCount++; // increment if a vowel exists
+                        }
+                    }
+                    if (vowelCount > vowelCountFinal) // If the current word has more vowels than the previous word
+                    {
+                        vowelCountFinal = vowelCount; // Vowel counter is raised
+                        vowelWordSort = wordList; // Word with most vowels is assigned
+                    }
+                    vowelWord.Text = vowelWordSort; // Print word with most vowels to textbox
+
+                    
+
+                    
+                }
+                
+
+                outputFile.WriteLine("First word alphabetically:");
+                outputFile.WriteLine(firstWordSort);
+
+                outputFile.WriteLine("Last word alphabetically:");
+                outputFile.WriteLine(lastWordSort);
+
+                outputFile.WriteLine("Word with most vowels:");
+                outputFile.WriteLine(vowelWordSort);
+                outputFile.Close();
+            }
+            else
+            {
+                MessageBox.Show("No file selected.");
+            }
+        }
+    }
+}
